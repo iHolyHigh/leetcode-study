7eeedb4fab0f2206aa4b8adaace866004775c7dd
HolyHigh <845803085@qq.com>
2020-01-06 22:42:30 +0800

    feat: leetcode算法学习，项目初始化

---
+++ src/main/java/com/holyhigh/leetcodestudy/LeetcodeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.holyhigh.leetcodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holyhigh/leetcodestudy/LeetcodeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holyhigh.leetcodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
