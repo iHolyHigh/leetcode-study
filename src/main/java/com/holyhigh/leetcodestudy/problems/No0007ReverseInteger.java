b00833c5e3a690ff3f20186fd6c620ebfae67bcd
HolyHigh <845803085@qq.com>
2020-01-10 22:52:17 +0800

    refactor: No0007-整数反转

--- src/main/java/com/holyhigh/leetcodestudy/problems/No0007ReverseInteger.java
+++ src/main/java/com/holyhigh/leetcodestudy/problems/No0007ReverseInteger.java
@@ -15,10 +15,8 @@ class No0007ReverseInteger {
         if (isNegative) {
             len -= 1;
         }
-        for (int i = 0; i < len; i++) {
-            int y = x % 10;
-            result = result * 10 + y;
-            x /= 10;
+        for (int i = 0; i < len; i++, x /= 10) {
+            result = result * 10 + x % 10;
         }
         if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
             return 0;
