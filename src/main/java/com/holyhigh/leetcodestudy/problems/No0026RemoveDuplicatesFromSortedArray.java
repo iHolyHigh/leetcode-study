7990bbdcc95b0c7f59a2f5110a38e31c83d70aed
HolyHigh <845803085@qq.com>
2020-01-12 16:08:12 +0800

    feat: No0026-删除排序数组中的重复项

---
+++ src/main/java/com/holyhigh/leetcodestudy/problems/No0026RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,29 @@
+package com.holyhigh.leetcodestudy.problems;
+
+/**
+ * No0026-删除排序数组中的重复项
+ *
+ * @author holyhigh
+ * @version 0.0.1
+ * @since 2020-01-12 00:28
+ */
+class No0026RemoveDuplicatesFromSortedArray {
+    int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        int result = 1;
+        int base = nums[0];
+        for (int i = 1, j = 1; i < nums.length; i++) {
+            if (nums[i] != base) {
+                nums[result] = nums[i];
+                base = nums[i];
+                result++;
+            }
+        }
+        return result;
+    }
+}
