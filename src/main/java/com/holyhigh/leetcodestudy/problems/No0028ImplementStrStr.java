7f86ada4ff9ce4a9b5d21df065715cd1154b70fa
HolyHigh <845803085@qq.com>
2020-01-15 00:21:23 +0800

    feat: No0028-实现strStr()

---
+++ src/main/java/com/holyhigh/leetcodestudy/problems/No0028ImplementStrStr.java
@@ -0,0 +1,14 @@
+package com.holyhigh.leetcodestudy.problems;
+
+/**
+ * No0028-实现strStr()
+ *
+ * @author holyhigh
+ * @version 0.0.1
+ * @since 2020-01-12 21:48
+ */
+class No0028ImplementStrStr {
+    int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
