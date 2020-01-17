b4022b5fae62eacf477e777d5ed77ed79d2ba1b8
HolyHigh <845803085@qq.com>
2020-01-17 23:40:01 +0800

    test: No0021-合并两个有序链表

---
+++ src/test/java/com/holyhigh/leetcodestudy/problems/No0021MergeTwoSortedListsTest.java
@@ -0,0 +1,71 @@
+package com.holyhigh.leetcodestudy.problems;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * No0021-合并两个有序链表
+ *
+ * @author holyhigh
+ * @version 0.0.1
+ * @since 2020-01-11 22:22
+ */
+class No0021MergeTwoSortedListsTest {
+    void assertEquals(Integer[] i1, Integer[] i2, Integer[] i3) {
+        No0021MergeTwoSortedLists mergeTwoSortedLists = new No0021MergeTwoSortedLists();
+        ListNode actual = mergeTwoSortedLists.mergeTwoLists(this.getListNode(i1), this.getListNode(i2));
+        this.assertEquals(this.getListNode(i3), actual);
+    }
+
+    ListNode getListNode(Integer[] integers) {
+        ListNode node = new ListNode(integers[0]);
+        ListNode current = node;
+        for (int i = 1; i < integers.length; i++) {
+            current = current.next = new ListNode(integers[i]);
+        }
+        return node;
+    }
+
+    void assertEquals(ListNode excepted, ListNode actual) {
+        while (excepted != null && actual != null) {
+            Assertions.assertEquals(excepted.val, actual.val);
+            excepted = excepted.next;
+            actual = actual.next;
+        }
+        Assertions.assertNull(excepted);
+        Assertions.assertNull(actual);
+
+    }
+
+    @Test
+    void testMergeTwoLists1() {
+        Integer[] i1 = {1, 2, 4};
+        Integer[] i2 = {1, 3, 4};
+        Integer[] i3 = {1, 1, 2, 3, 4, 4};
+        this.assertEquals(i1, i2, i3);
+    }
+
+    @Test
+    void testMergeTwoLists2() {
+        Integer[] i1 = {1};
+        Integer[] i2 = {4};
+        Integer[] i3 = {1, 4};
+        this.assertEquals(i1, i2, i3);
+    }
+
+    @Test
+    void testMergeTwoLists3() {
+        Integer[] i1 = {1, 2};
+        Integer[] i2 = {4, 5, 6};
+        Integer[] i3 = {1, 2, 4, 5, 6};
+        this.assertEquals(i1, i2, i3);
+    }
+
+    @Test
+    void testMergeTwoLists4() {
+        Integer[] i1 = {1, 3, 5};
+        Integer[] i2 = {2, 4, 6};
+        Integer[] i3 = {1, 2, 3, 4, 5, 6};
+        this.assertEquals(i1, i2, i3);
+    }
+}
