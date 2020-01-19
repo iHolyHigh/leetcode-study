8218b127ab59abf699d185f04d5a679d56cf13c7
HolyHigh <845803085@qq.com>
2020-01-19 21:19:32 +0800

    test: No0021-合并两个有序链表

--- src/main/java/com/holyhigh/leetcodestudy/problems/ListNode.java
+++ src/main/java/com/holyhigh/leetcodestudy/problems/ListNode.java
@@ -1,5 +1,7 @@
 package com.holyhigh.leetcodestudy.problems;
 
+import java.util.Objects;
+
 /**
  * Definition for singly-linked list.
  *
@@ -9,9 +11,44 @@ package com.holyhigh.leetcodestudy.problems;
  */
 class ListNode {
     int val;
+
     ListNode next;
 
     ListNode(int x) {
         val = x;
+    }
+
+    static ListNode getListNode(int[] ints) {
+        ListNode head = new ListNode(-1);
+        ListNode current = head;
+        for (int i : ints) {
+            current = current.next = new ListNode(i);
+        }
+        return head.next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ListNode listNode = (ListNode) o;
+        ListNode current = this;
+        while (listNode != null && current != null) {
+            if (listNode.val != current.val) {
+                return false;
+            }
+            listNode = listNode.next;
+            current = current.next;
+        }
+        return listNode == null && current == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
     }
 }
--- src/test/java/com/holyhigh/leetcodestudy/problems/No0021MergeTwoSortedListsTest.java
+++ src/test/java/com/holyhigh/leetcodestudy/problems/No0021MergeTwoSortedListsTest.java
@@ -11,60 +11,41 @@ import org.junit.jupiter.api.Test;
  * @since 2020-01-11 22:22
  */
 class No0021MergeTwoSortedListsTest {
-    void assertEquals(int[] i1, int[] i2, int[] i3) {
+    void assertEquals(ListNode l1, ListNode l2, ListNode excepted) {
         No0021MergeTwoSortedLists mergeTwoSortedLists = new No0021MergeTwoSortedLists();
-        ListNode actual = mergeTwoSortedLists.mergeTwoLists(this.getListNode(i1), this.getListNode(i2));
-        this.assertEquals(this.getListNode(i3), actual);
-    }
-
-    ListNode getListNode(int[] ints) {
-        ListNode node = new ListNode(ints[0]);
-        ListNode current = node;
-        for (int i = 1; i < ints.length; i++) {
-            current = current.next = new ListNode(ints[i]);
-        }
-        return node;
-    }
-
-    void assertEquals(ListNode excepted, ListNode actual) {
-        while (excepted != null && actual != null) {
-            Assertions.assertEquals(excepted.val, actual.val);
-            excepted = excepted.next;
-            actual = actual.next;
-        }
-        Assertions.assertNull(excepted);
-        Assertions.assertNull(actual);
+        ListNode actual = mergeTwoSortedLists.mergeTwoLists(l1, l2);
+        Assertions.assertEquals(excepted, actual);
     }
 
     @Test
     void testMergeTwoLists1() {
-        int[] i1 = {1, 2, 4};
-        int[] i2 = {1, 3, 4};
-        int[] i3 = {1, 1, 2, 3, 4, 4};
-        this.assertEquals(i1, i2, i3);
+        ListNode l1 = ListNode.getListNode(new int[]{1, 2, 4});
+        ListNode l2 = ListNode.getListNode(new int[]{1, 3, 4});
+        ListNode excepted = ListNode.getListNode(new int[]{1, 1, 2, 3, 4, 4});
+        this.assertEquals(l1, l2, excepted);
     }
 
     @Test
     void testMergeTwoLists2() {
-        int[] i1 = {1};
-        int[] i2 = {4};
-        int[] i3 = {1, 4};
-        this.assertEquals(i1, i2, i3);
+        ListNode l1 = ListNode.getListNode(new int[]{1});
+        ListNode l2 = ListNode.getListNode(new int[]{4});
+        ListNode excepted = ListNode.getListNode(new int[]{1, 4});
+        this.assertEquals(l1, l2, excepted);
     }
 
     @Test
     void testMergeTwoLists3() {
-        int[] i1 = {1, 2};
-        int[] i2 = {4, 5, 6};
-        int[] i3 = {1, 2, 4, 5, 6};
-        this.assertEquals(i1, i2, i3);
+        ListNode l1 = ListNode.getListNode(new int[]{1, 2});
+        ListNode l2 = ListNode.getListNode(new int[]{4, 5, 6});
+        ListNode excepted = ListNode.getListNode(new int[]{1, 2, 4, 5, 6});
+        this.assertEquals(l1, l2, excepted);
     }
 
     @Test
     void testMergeTwoLists4() {
-        int[] i1 = {1, 3, 5};
-        int[] i2 = {2, 4, 6};
-        int[] i3 = {1, 2, 3, 4, 5, 6};
-        this.assertEquals(i1, i2, i3);
+        ListNode l1 = ListNode.getListNode(new int[]{1, 3, 5});
+        ListNode l2 = ListNode.getListNode(new int[]{2, 4, 6});
+        ListNode excepted = ListNode.getListNode(new int[]{1, 2, 3, 4, 5, 6});
+        this.assertEquals(l1, l2, excepted);
     }
 }
